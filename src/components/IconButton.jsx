501f023825f24ec82e7cbee6a21e9c07f1de0d80
Fenyx <64859587+steven-van@users.noreply.github.com>
2025-12-05 23:54:51 +0100

    fix: refacto

--- src/components/IconButton.jsx
+++ src/components/IconButton.jsx
@@ -1,8 +1,8 @@
 import { Icon } from "@iconify/react";
 
-const IconButton = ({ label, icon, className, onClick }) => {
+const IconButton = ({ label, icon, onClick }) => {
   return (
-    <button className={className} onClick={onClick}>
+    <button className={`icon-btn`} onClick={onClick}>
       <Icon icon={icon} width="20" height="20" />
       {label}
     </button>
