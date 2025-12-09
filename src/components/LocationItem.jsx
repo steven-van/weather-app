ba67d0d456d9f898754a0b9debe8cf30023ed8d3
Fenyx <64859587+steven-van@users.noreply.github.com>
2025-12-09 21:38:53 +0100

    feat: locationitem

---
+++ src/components/LocationItem.jsx
@@ -0,0 +1,23 @@
+const LocationItem = ({ location, isSelected, onClick }) => {
+  const { name, country, country_code } = location;
+  const countryCode = country_code.toLowerCase();
+
+  return (
+    <button
+      className={`location-item ${isSelected ? "active" : ""}`}
+      onClick={onClick}
+    >
+      <img
+        className="country-flag"
+        src={`https://flagcdn.com/${countryCode}.svg`}
+        width="30"
+        alt={country}
+      />
+      <span>
+        {name}, {country}
+      </span>
+    </button>
+  );
+};
+
+export default LocationItem;
